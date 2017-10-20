1529c2d42d908a6a57b9e5721137d43c930099db
liuhao <58254451@qq.com>
2017-10-20 17:23:21 +0800

    server端增加获取计划、日志接口

--- server/app/controler/deviceinfo.py
+++ server/app/controler/deviceinfo.py
@@ -24,9 +24,9 @@ class deviceinfo:
 		else:
 			result='{"status":"400","body":"username does not exist"}'
 		cursor.close()
-		return result
+		#return result
 		mysql_r.close()
-
+		return result
 
 	@staticmethod
 	def get_device_info_log(sn,username,ip,ua,r):
@@ -40,3 +40,54 @@ class deviceinfo:
 		mysql_r.commit()
 		cursor.close()
 		mysql_r.close()
+
+	@staticmethod
+	def get_plan(game_id,channel_type,token,ip):
+ 		mysql_r = pymysql.connect(host=Config.mysql_conf['host'],port=Config.mysql_conf['port'],user=Config.mysql_conf['user'],password=Config.mysql_conf['password'],database=Config.mysql_conf['dbName'],charset=Config.mysql_conf['charset'])
+ 		account_id="none"
+ 		username="none"
+		result=""
+		sql='select username,password,channel_name,server_id,action_type,648_,328_,228_,198_,98_,30_,6_,account_id,game_id from zilong_robot.`account_info` where status=0 and channel_name="'+str(channel_type)+'" and game_id="'+str(game_id)+'" order by DATE_FORMAT( plan_time,"%Y-%c-%d %h:%i:%s") limit 1'
+		print(sql)
+		cursor=mysql_r.cursor()
+		cursor.execute(sql)
+		rs=cursor.fetchall()		
+		if len(rs)>0:
+			for r in rs:
+				result='[{"game_id":"'+str(r[13])+'","username":"'+str(r[0])+'","password":"'+str(r[1])+'","channel_name":"'+str(r[2])+'","server_id":"'+str(r[3])+'","action_type":"'+str(r[4])+'","648_":"'+str(r[5])+'","328_":"'+str(r[6])+'","228_":"'+str(r[7])
+				result+='","198_":"'+str(r[8])+'","98_":"'+str(r[9])+'","30_":"'+str(r[10])+'","6_":"'+str(r[11])+'"}]'
+				result='{"status":"200","body":'+result+'}'
+				account_id=str(r[12])
+				username=str(r[0])
+			sql='insert into zilong_robot.account_run_log values("'+str(account_id)+'","'+str(username)+'",now(),"","1","action_start","'+str(token)+'","'+str(ip)+'","");'
+			sql+='update zilong_robot.account_info set status=1 where account_id="'+str(account_id)+'";'
+			print(sql)
+			cursor.execute(sql)
+			mysql_r.commit()
+
+		else:
+			result='{"status":"400","body":"there is no plan now"}'
+		cursor.close()
+		#return result
+		mysql_r.close()
+		return result		
+	@staticmethod
+	def update_status(status,des,accountid,ip_):
+ 		mysql_r = pymysql.connect(host=Config.mysql_conf['host'],port=Config.mysql_conf['port'],user=Config.mysql_conf['user'],password=Config.mysql_conf['password'],database=Config.mysql_conf['dbName'],charset=Config.mysql_conf['charset'])
+ 		try: 
+	 		account_id="none"
+	 		username="none"
+			result=""
+			sql='update account_run_log set end_time=now(),status="'+str(status)+'",des="'+str(des)+'",end_IP="'+str(ip_)+'" where account_id="'+str(accountid)+'";'
+			sql+='update account_info set status="'+str(status)+'" where account_id="'+str(accountid)+'"' 
+			print(sql)
+			cursor=mysql_r.cursor()
+			cursor.execute(sql)
+			mysql_r.commit()
+			result=	'{"status":"200"}'
+		except:
+			result='{"status":"400","body":"sql is error"}'
+		cursor.close()
+		#return result
+		mysql_r.close()
+		return result
--- server/manager.py
+++ server/manager.py
@@ -30,6 +30,60 @@ def getdeviceinfo():
 	deviceinfo.get_device_info_log(sn,username,ip_,UA_[0:199],r[11:14])
 	return str(r)
 
+@app.route('/getplan')
+#getLP?creativeID=838
+def getplan_():
+	r=""
+	sn=""
+	channel_type=""
+	game_id=""
+	ip_=request.remote_addr
+	UA_=request.headers.get('User-Agent')
+	args=request.args.items()
+	for x in xrange(0,len(args)):
+		if args[x][0].lower()=="sn".lower():
+			sn=args[x][1]
+		if args[x][0].lower()=="game_id".lower():
+			game_id=args[x][1]
+		if args[x][0].lower()=="channel_type".lower():
+			channel_type=args[x][1]
+	if  channel_type=="" or channel_type is None  or len(channel_type)>50 or game_id=="" or game_id is None :
+		r='{"status":"500"}'
+	else:
+		r=deviceinfo.get_plan(game_id,channel_type,sn,ip_)
+	return str(r)
+
+
+
+
+
+@app.route('/update_status')
+
+def update_status_():
+	r=""
+	status=""
+	des=""
+	account_id=""
+	ip_=request.remote_addr
+	# UA_=request.headers.get('User-Agent')
+	args=request.args.items()
+	for x in xrange(0,len(args)):
+		if args[x][0].lower()=="status".lower():
+			print(args[x][1])
+			status=re.sub("[^\w]+","",args[x][1])
+		if args[x][0].lower()=="des".lower():
+			print(args[x][1])
+			des=re.sub("[^\w]+","",args[x][1])
+		if args[x][0].lower()=="account_id".lower():
+			print(args[x][1])
+			account_id=re.sub("[^\w]+","",args[x][1])
+	if (status!="4" and status!="2") or account_id=="" or account_id is None:
+		print 1
+		r='{"status":"500"}'
+	else:
+		r=deviceinfo.update_status(status[0:10],des[0:40],account_id[0:15],ip_)
+	
+	return str(r)
 
 
 if __name__ == '__main__':
